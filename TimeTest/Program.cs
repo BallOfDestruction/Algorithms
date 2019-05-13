6d0f42c52a633416e6620ccd459b06254836b869
asura <theworldtocollapse@mail.ru>
2019-05-13 22:09:24 +0300

    sortings tests

--- TimeTest/Program.cs
+++ TimeTest/Program.cs
@@ -1,28 +1,16 @@
 ﻿using System;
-using Core.Generators;
-using Core.Sorting;
 
 namespace TimeTest
 {
     internal class Program
     {
-        private const int CountWorks = 10;
-        private const int ItemsCount = 10000;
+        private const int WorksCount = 10;
+        private const int ItemsCount = 1000;
         
         public static void Main(string[] args)
         {
-            Console.WriteLine($"Count: {CountWorks}");
-            
-            var stopWatch = new WorkStopWatch();
-
-            Console.WriteLine();
-            Console.WriteLine($"Sorting. Items count: {ItemsCount}");
-            
-            var randomGenerator = new IntRandomGenerator();
-            
-            var bubbleSorting = new BubbleSorting<int>();
-            var time = stopWatch.CheckTime(() => randomGenerator.Generate(ItemsCount), ints => bubbleSorting.DoSort(ints), CountWorks);
-            Console.WriteLine($"{bubbleSorting.Name}: {time} ms");
+            var sortingTimeTest = new SortingTimeTest();
+            sortingTimeTest.Do(ItemsCount, WorksCount);
 
             Console.WriteLine("Press any key...");
             Console.ReadKey();
---
+++ TimeTest/SortingTimeTest.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using Core.Generators;
+using Core.Sorting;
+using JetBrains.Annotations;
+
+namespace TimeTest
+{
+    public class SortingTimeTest
+    {
+        [NotNull] private List<ISorting<int>> _sortings = new List<ISorting<int>>
+        {
+            new BubbleSorting<int>(),
+            new BubbleSorting<int>(),
+        };
+        
+        public void Do(int itemsCount, int worksCount)
+        {
+            var stopWatch = new WorkStopWatch();
+
+            Console.WriteLine();
+            Console.WriteLine("Sorting");
+            Console.WriteLine($"Items count: {itemsCount}");
+            Console.WriteLine($"Works count: {worksCount}");
+
+            var randomGenerator = new IntRandomGenerator();
+
+            foreach (var sorting in _sortings)
+            {
+                var time = stopWatch.CheckTime(() => randomGenerator.Generate(itemsCount),
+                    ints => sorting.DoSort(ints),
+                    worksCount);
+            
+                Console.WriteLine();
+                Console.WriteLine($"{sorting.Name}:");
+                Console.WriteLine($"Common time: {time} ms");
+                Console.WriteLine($"Average time: {(float) time / worksCount} ms");
+            }
+            
+            Console.WriteLine();
+        }
+    }
+}
